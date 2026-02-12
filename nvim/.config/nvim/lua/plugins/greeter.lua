634e71b1327bb981305ded7483bfeded56f68c3f
cytzxcv <mail.cyt@pm.me>
2026-02-12 20:38:34 +0800

    added keybinds to sleep and hibernation

--- nvim/.config/nvim/lua/plugins/greeter.lua
+++ nvim/.config/nvim/lua/plugins/greeter.lua
@@ -1,6 +1,7 @@
 return {{
     "goolord/alpha-nvim",
     config = function()
-        require("alpha").setup(require("alpha.themes.dashboard").config)
+        dashboard = require("alpha.themes.dashboard")
+        require("alpha").setup(dashboard.config)
     end
 }}
